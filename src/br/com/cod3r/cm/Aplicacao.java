77d713316b80139fa0b4262085b1f2ab046206f5
Guilherme.Granato <guilherme.granato@ab-inbev.com>
2022-06-16 09:30:44 -0300

    updating the console interaction

--- src/br/com/cod3r/cm/Aplicacao.java
+++ src/br/com/cod3r/cm/Aplicacao.java
@@ -1,17 +1,13 @@
 package br.com.cod3r.cm;
 
 import br.com.cod3r.cm.modelo.Tabuleiro;
+import br.com.cod3r.cm.visao.TabuleiroConsole;
 
 import java.util.Random;
 
 public class Aplicacao {
     public static void main(String[] args) {
         Tabuleiro tabuleiro = new Tabuleiro(6,6,6);
-
-        tabuleiro.marcar(4,4);
-        tabuleiro.marcar(5,5);
-        tabuleiro.abrir(3,3);
-        tabuleiro.abrir(1,1);
-        System.out.println(tabuleiro);
+        new TabuleiroConsole(tabuleiro);
     }
 }
